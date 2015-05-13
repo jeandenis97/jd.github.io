ff2acc44f96c081fea1627c784746454dd676077
jeandenis97 <t2zik.973@gmail.Com>
2015-05-13 17:01:38 +0200

    premier

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jdgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jdgithubioApp
+ */
+angular.module('jdgithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jdgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jdgithubioApp
+ */
+angular.module('jdgithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
